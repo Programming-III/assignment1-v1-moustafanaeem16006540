c6ea0fd9e159f57adaf36081450256acbe7dd40c
moustafamaged <moustafamaged874@gmail.com>
2025-11-12 15:11:47 +0200

    Update Mammal.h

--- Mammal.h
+++ Mammal.h
@@ -2,6 +2,21 @@
 #define MAMMAL_H
 
 #include "Animal.h"
+#include <iostream>
+using namespace std;
+
+class Mammal : public Animal {
+private:
+    string furColor;
+
+public:
+    Mammal();
+    Mammal(string n, int a, bool h, string c);
+    void display() override;
+     ~Mammal();
+};
+
+#endif
 
 //define mammal class here
 
