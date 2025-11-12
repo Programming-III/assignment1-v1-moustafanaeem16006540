48f233cf4669ee51db76071c6fbe5ec0ae62873a
moustafamaged <moustafamaged874@gmail.com>
2025-11-12 15:08:58 +0200

    Add Animal class definition with constructors and methods

--- Animal.h
+++ Animal.h
@@ -1,11 +1,22 @@
 #ifndef ANIMAL_H
 #define ANIMAL_H
 
-#include <string>
+#include <iostream>
 using namespace std;
 
-//define animal class here 
+class Animal {
+private:
+    string name;
+    int age;
+    bool isHungry;
 
+public:
+    Animal();
+    Animal(string n, int a, bool h);
+    virtual void display();
+    virtual void feed();
+     virtual ~Animal();
+};
 
 
 #endif
