262701f4eac068e03d0e755bfde77d78dde9d515
moustafamaged <moustafamaged874@gmail.com>
2025-11-12 15:12:57 +0200

    Update Bird.h

--- Bird.h
+++ Bird.h
@@ -1,7 +1,21 @@
 #ifndef BIRD_H
 #define BIRD_H
-
 #include "Animal.h"
+#include <iostream>
+using namespace std;
+
+class Bird : public Animal {
+private:
+    float wingSpan;
+
+public:
+    Bird();
+    Bird(string n, int a, bool h, float s);
+    void display() override;
+~Bird();
+};
+
+#endif
 
 //define class bird here 
 
