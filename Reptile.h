a1e2c9c6dc8dbccdf313e2d7a6ff34c037dc7d08
moustafamaged <moustafamaged874@gmail.com>
2025-11-12 15:23:50 +0200

    Update Reptile.h

--- Reptile.h
+++ Reptile.h
@@ -5,6 +5,24 @@
 
 //define reptile class here
 
+#include "Animal.h"
+#include <iostream>
+using namespace std;
+
+class Reptile : public Animal {
+private:
+    bool isVenomous;
+
+public:
+    Reptile();
+    Reptile(string n, int a, bool h, bool v);
+    void display() override;
+    ~Reptile();
+
+};
+
+#endif
+
 
 
 
